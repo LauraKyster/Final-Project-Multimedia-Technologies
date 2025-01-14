e33fa9f2fff2aeefd540bf73cdf9107bcd294421
Laura Marie Kyster <laky0001@stud.kea.dk>
2025-01-14 17:40:05 +0100

    js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Hent elementer
+const overlay = document.getElementById("overlay");
+const openBtn = document.getElementById("openOverlay");
+const closeBtn = document.getElementById("closeOverlay");
+console.log(openBtn);
+// Åbn overlayet
+openBtn.addEventListener("click", () => {
+  overlay.classList.remove("hidden");
+  setTimeout(function () {
+    overlay.style.opacity = 1;
+  }, 50);
+});
+/* slower display? */
+
+// Luk overlayet
+closeBtn.addEventListener("click", () => {
+  overlay.classList.add("hidden");
+  overlay.style.opacity = 0;
+});
+
+// Luk overlayet ved klik udenfor boksen
+overlay.addEventListener("click", (event) => {
+  overlay.classList.add("hidden");
+});
+
+window.addEventListener("scroll", function () {
+  console.log(this.window.scrollY);
+  if (this.window.scrollY <= 50) {
+    this.document.querySelector("header").classList.add("menu-background");
+  } else {
+    this.document.querySelector("header").classList.remove("menu-background");
+  }
+});
